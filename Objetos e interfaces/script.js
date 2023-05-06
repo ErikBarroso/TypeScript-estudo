26fd5bad45e6986dab842e568d03ad568fd2c97a
Erik <barroso.erik@live.com>
2023-05-06 20:39:38 -0300

    estudos sobre instanceof

--- Objetos e interfaces/script.js
+++ Objetos e interfaces/script.js
@@ -1,14 +1,18 @@
 "use strict";
-class Produto {
-    nome;
-    preco;
-    constructor(nomeDoProduto, precoDoProduto) {
-        this.nome = nomeDoProduto;
-        this.preco = precoDoProduto;
-    }
-    precoReal() {
-        return `R$ ${this.preco}`;
-    }
+// class Produto {
+//   nome: string;
+//   preco: number;
+//   constructor(nomeDoProduto: string, precoDoProduto: number) {
+//     this.nome = nomeDoProduto;
+//     this.preco = precoDoProduto;
+//   }
+//   precoReal() {
+//     return `R$ ${this.preco}`;
+//   }
+// }
+// const livro = new Produto("O Grande Conflito", 500);
+// console.log(livro.nome, livro.preco, livro.precoReal());
+const link = document.getElementById("origamid");
+if (link instanceof HTMLAnchorElement) {
+    link.href = link.href.replace("http://", "https://");
 }
-const livro = new Produto("O Grande Conflito", 500);
-console.log(livro.nome, livro.preco, livro.precoReal());
