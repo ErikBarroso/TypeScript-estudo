8c2ef66aacca0aec9fdd9cc0cc603b0a87056d69
Erik <barroso.erik@live.com>
2023-05-09 17:45:23 -0300

    Aprendendo sobre TypeGuard

--- Objetos e interfaces/script.js
+++ Objetos e interfaces/script.js
@@ -16,13 +16,44 @@
 // if (link instanceof HTMLAnchorElement) {
 //   link.href = link.href.replace("http://", "https://");
 // }
-const links = document.querySelectorAll(".link");
-links.forEach((item) => {
-    if (item instanceof HTMLAnchorElement) {
-        ativarElemento(item);
+// const links = document.querySelectorAll(".link");
+// links.forEach((item) => {
+//   if (item instanceof HTMLAnchorElement) {
+//     ativarElemento(item);
+//   }
+// });
+// function ativarElemento(elemento: HTMLAnchorElement) {
+//   elemento.style.color = "red";
+//   elemento.style.border = "12px solid red";
+// }
+async function fetchCursos() {
+    const response = await fetch("https://api.origamid.dev/json/cursos.json");
+    const json = await response.json();
+    handleCursos(json);
+}
+fetchCursos();
+function isCurso(value) {
+    if (value &&
+        typeof value === "object" &&
+        "nome" in value &&
+        "horas" in value &&
+        "tags" in value) {
+        return true;
     }
-});
-function ativarElemento(elemento) {
-    elemento.style.color = "red";
-    elemento.style.border = "12px solid red";
+    else {
+        return false;
+    }
+}
+function handleCursos(data) {
+    if (Array.isArray(data)) {
+        data.filter(isCurso).forEach((item) => {
+            document.body.innerHTML += `
+      <div> 
+      <h2>${item.nome}</h2>
+      <p>${item.horas}</p>
+      </div>
+      
+      `;
+        });
+    }
 }
